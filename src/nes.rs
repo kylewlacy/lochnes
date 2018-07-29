beed06d25bab200746cabaceefaa07a962765219
Kyle Lacy <kylelacy+git@pm.me>
2018-07-29 02:57:17 -0700

    Implement several common opcodes
    Added opcodes include AND, BEQ, BNE, BPL, DEC, DEY, JMP, JSR, LDY, STA, and STY (not all variants)

--- src/nes.rs
+++ src/nes.rs
@@ -52,6 +52,10 @@ impl Nes {
         }
     }
 
+    pub fn read_i8(&self, addr: u16) -> i8 {
+        self.read_u8(addr) as i8
+    }
+
     pub fn read_u16(&self, addr: u16) -> u16 {
         let lo = self.read_u8(addr);
         let hi = self.read_u8(addr + 1);
@@ -98,10 +102,112 @@ impl Nes {
 
         let op;
         match opcode {
+            Opcode::AndImm => {
+                let a = self.cpu.a;
+                let value = self.read_u8(pc + 1);
+                let a = a & value;
+                self.cpu.a = a;
+
+                self.cpu.set_flags(CpuFlags::Z, a == 0);
+                self.cpu.set_flags(CpuFlags::N, (a & 0b_1000_0000) != 0);
+
+                next_pc = pc + 2;
+                op = Op::AndImm { value };
+            }
+            Opcode::Beq => {
+                let addr_offset = self.read_i8(pc + 1);
+
+                let pc_after = pc + 2;
+                if self.cpu.p.contains(CpuFlags::Z) {
+                    // TODO: Handle offset past page! With that, `i8` shouldn't
+                    // be necessary
+                    next_pc = (pc_after as i16 + addr_offset as i16) as u16;
+                }
+                else {
+                    next_pc = pc_after;
+                }
+                op = Op::Beq { addr_offset };
+            }
+            Opcode::Bne => {
+                let addr_offset = self.read_i8(pc + 1);
+
+                let pc_after = pc + 2;
+                if self.cpu.p.contains(CpuFlags::Z) {
+                    next_pc = pc_after;
+                }
+                else {
+                    // TODO: Handle offset past page! With that, `i8` shouldn't
+                    // be necessary
+                    next_pc = (pc_after as i16 + addr_offset as i16) as u16;
+                }
+                op = Op::Bne { addr_offset };
+            }
+            Opcode::Bpl => {
+                let addr_offset = self.read_i8(pc + 1);
+
+                let pc_after = pc + 2;
+                if self.cpu.p.contains(CpuFlags::N) {
+                    next_pc = pc_after;
+                }
+                else {
+                    // TODO: Handle offset past page! With that, `i8` shouldn't
+                    // be necessary
+                    next_pc = (pc_after as i16 + addr_offset as i16) as u16;
+                }
+                op = Op::Bpl { addr_offset };
+            }
             Opcode::Cld => {
                 self.cpu.set_flags(CpuFlags::D, false);
                 next_pc = pc + 1;
                 op = Op::Cld;
+            }
+            Opcode::DecZero => {
+                let zero_page = self.read_u8(pc + 1);
+                let addr = zero_page as u16;
+                let value = self.read_u8(addr);
+                let value = value.wrapping_sub(1);
+                self.write_u8(addr, value);
+
+                self.cpu.set_flags(CpuFlags::Z, value == 0);
+                self.cpu.set_flags(CpuFlags::N, (value & 0b_1000_0000) != 0);
+
+                next_pc = pc + 2;
+                op = Op::DecZero { zero_page };
+            }
+            Opcode::Dey => {
+                let y = self.cpu.y.wrapping_sub(1);
+                self.cpu.set_flags(CpuFlags::Z, y == 0);
+                self.cpu.set_flags(CpuFlags::N, (y & 0b_1000_0000) != 0);
+                self.cpu.y = y;
+
+                next_pc = pc + 1;
+                op = Op::Dey;
+            }
+            Opcode::JmpAbs => {
+                let addr = self.read_u16(pc + 1);
+
+                next_pc = addr;
+                op = Op::JmpAbs { addr };
+            }
+            Opcode::Jsr => {
+                let addr = self.read_u16(pc + 1);
+                let ret_pc = pc.wrapping_add(3);
+                let push_pc = ret_pc.wrapping_sub(1);
+                let push_pc_hi = ((0xFF00 & push_pc) >> 8) as u8;
+                let push_pc_lo = (0x00FF & push_pc) as u8;
+                let stack_offset_hi = self.cpu.s;
+                let stack_offset_lo = self.cpu.s.wrapping_sub(1);
+                let stack_addr_hi = 0x0100 & stack_offset_hi as u16;
+                let stack_addr_lo = 0x0100 & stack_offset_lo as u16;
+
+                self.write_u8(stack_addr_hi, push_pc_hi);
+                self.write_u8(stack_addr_lo, push_pc_lo);
+
+                self.cpu.s = self.cpu.s.wrapping_sub(2);
+
+                next_pc = addr;
+                op = Op::Jsr { addr };
+
             }
             Opcode::LdaAbs => {
                 let addr = self.read_u16(pc + 1);
@@ -122,6 +228,12 @@ impl Nes {
                 next_pc = pc + 2;
                 op = Op::LdxImm { value };
             }
+            Opcode::LdyImm => {
+                let value = self.read_u8(pc + 1);
+                self.cpu.y = value;
+                next_pc = pc + 2;
+                op = Op::LdyImm { value };
+            }
             Opcode::Sei => {
                 self.cpu.set_flags(CpuFlags::I, true);
                 next_pc = pc + 1;
@@ -133,6 +245,36 @@ impl Nes {
                 self.write_u8(addr, a);
                 next_pc = pc + 3;
                 op = Op::StaAbs { addr };
+            }
+            Opcode::StaZero => {
+                let a = self.cpu.a;
+                let zero_page = self.read_u8(pc + 1);
+                let addr = zero_page as u16;
+                self.write_u8(addr, a);
+                next_pc = pc + 2;
+                op = Op::StaZero { zero_page };
+            }
+            Opcode::StaIndY => {
+                let a = self.cpu.a;
+                let y = self.cpu.y;
+                let target_addr_base = self.read_u8(pc + 1);
+
+                // TODO: Is this right? Does the target address
+                // wrap around the zero page?
+                let addr_base = self.read_u16(target_addr_base as u16);
+                let addr = addr_base.wrapping_add(y as u16);
+                self.write_u8(addr, a);
+
+                next_pc = pc + 2;
+                op = Op::StaIndY { target_addr_base };
+            }
+            Opcode::StyZero => {
+                let y = self.cpu.y;
+                let zero_page = self.read_u8(pc + 1);
+                let addr = zero_page as u16;
+                self.write_u8(addr, y);
+                next_pc = pc + 2;
+                op = Op::StyZero { zero_page };
             }
             Opcode::Txs => {
                 let x = self.cpu.x;
@@ -244,25 +386,49 @@ bitflags! {
 #[derive(Debug, EnumKind)]
 #[enum_kind(Opcode)]
 pub enum Op {
+    AndImm { value: u8 },
+    Beq { addr_offset: i8 },
+    Bne { addr_offset: i8 },
+    Bpl { addr_offset: i8 },
     Cld,
+    DecZero { zero_page: u8 },
+    Dey,
+    JmpAbs { addr: u16 },
+    Jsr { addr: u16 },
     LdaAbs { addr: u16 },
     LdaImm { value: u8 },
     LdxImm { value: u8 },
+    LdyImm { value: u8 },
     Sei,
     StaAbs { addr: u16 },
+    StaZero { zero_page: u8 },
+    StaIndY { target_addr_base: u8 },
+    StyZero { zero_page: u8 },
     Txs,
 }
 
 impl Opcode {
     fn from_u8(opcode: u8) -> Self {
         match opcode {
+            0x10 => Opcode::Bpl,
+            0x20 => Opcode::Jsr,
+            0x29 => Opcode::AndImm,
+            0x4C => Opcode::JmpAbs,
             0x78 => Opcode::Sei,
+            0x84 => Opcode::StyZero,
+            0x85 => Opcode::StaZero,
+            0x88 => Opcode::Dey,
             0x8D => Opcode::StaAbs,
+            0x91 => Opcode::StaIndY,
             0x9A => Opcode::Txs,
+            0xA0 => Opcode::LdyImm,
             0xA2 => Opcode::LdxImm,
             0xA9 => Opcode::LdaImm,
             0xAD => Opcode::LdaAbs,
+            0xC6 => Opcode::DecZero,
+            0xD0 => Opcode::Bne,
             0xD8 => Opcode::Cld,
+            0xF0 => Opcode::Beq,
             opcode => {
                 unimplemented!("Unhandled opcode: 0x{:X}", opcode);
             }
@@ -273,11 +439,21 @@ impl Opcode {
 impl fmt::Display for Opcode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mnemonic = match self {
+            Opcode::AndImm => "AND",
+            Opcode::Beq => "BEQ",
+            Opcode::Bne => "BNE",
+            Opcode::Bpl => "BPL",
             Opcode::Cld => "CLD",
+            Opcode::DecZero => "DEC",
+            Opcode::Dey => "DEY",
+            Opcode::JmpAbs => "JMP",
+            Opcode::Jsr => "JSR",
             Opcode::LdaAbs | Opcode::LdaImm => "LDA",
             Opcode::LdxImm => "LDX",
+            Opcode::LdyImm => "LDY",
             Opcode::Sei => "SEI",
-            Opcode::StaAbs => "STA",
+            Opcode::StaAbs | Opcode::StaZero | Opcode::StaIndY => "STA",
+            Opcode::StyZero => "STY",
             Opcode::Txs => "TXS",
         };
         write!(f, "{}", mnemonic)?;
@@ -289,14 +465,39 @@ impl fmt::Display for Op {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let opcode = Opcode::from(self);
         match self {
-            Op::Cld | Op::Sei | Op::Txs => {
+            Op::Cld | Op::Sei | Op::Txs | Op::Dey => {
                 write!(f, "{}", opcode)?;
             }
-            Op::LdaAbs { addr } | Op::StaAbs { addr } => {
+            Op::LdaAbs { addr }
+            | Op::StaAbs { addr }
+            | Op::JmpAbs { addr }
+            | Op::Jsr { addr } => {
                 write!(f, "{} ${:04X}", opcode, addr)?;
             }
-            Op::LdaImm { value } | Op::LdxImm { value } => {
+            Op::DecZero { zero_page }
+            | Op::StaZero { zero_page }
+            | Op::StyZero { zero_page }=> {
+                write!(f, "{} ${:04X}", opcode, *zero_page as u16)?;
+            }
+            Op::StaIndY { target_addr_base } => {
+                write!(f, "{} (${:02X}),Y", opcode, target_addr_base)?;
+            }
+            Op::AndImm { value }
+            | Op::LdaImm { value }
+            | Op::LdxImm { value }
+            | Op::LdyImm { value } => {
                 write!(f, "{} #${:02X}", opcode, value)?;
+            }
+            Op::Beq { addr_offset }
+            | Op::Bne { addr_offset }
+            | Op::Bpl { addr_offset } => {
+                if *addr_offset >= 0 {
+                    write!(f, "{} _ + #${:02X}", opcode, addr_offset)?;
+                }
+                else {
+                    let abs_offset = -(*addr_offset as i16);
+                    write!(f, "{} _ - #${:02X}", opcode, abs_offset)?;
+                }
             }
         }
         Ok(())
