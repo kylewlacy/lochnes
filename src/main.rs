fa95b67253d06d40d911ec0d34824e439c35d45d
Kyle Lacy <kylelacy+git@pm.me>
2018-08-19 22:09:49 -0700

    Add simple CPU benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-#![feature(nll, proc_macro_non_items, use_extern_macros, generators, never_type)]
+#![feature(nll, proc_macro_non_items, use_extern_macros, generators, never_type, tests)]
+#![cfg_attr(test, feature(test))]
 
 #[macro_use]
 extern crate bitflags;
 #[macro_use]
 extern crate enum_kinds;
 extern crate structopt;
+#[cfg(test)] extern crate test;
 
 use std::path::PathBuf;
 use std::process;
@@ -61,5 +63,33 @@ impl From<io::Error> for LochnesError {
 impl From<rom::RomError> for LochnesError {
     fn from(err: rom::RomError) -> Self {
         LochnesError::RomError(err)
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use std::{env, fs};
+    use test::Bencher;
+    use super::*;
+
+    #[bench]
+    fn bench_cycles(b: &mut Bencher) {
+        // TODO: Add a ROM as a fixture for benchmarking
+        let rom_path = env::var("BENCH_ROM").expect("BENCH_ROM env var must be set for benchmarking");
+        let rom_bytes = fs::read(rom_path).expect("Failed to open BENCH_ROM");
+        let rom = rom::Rom::from_bytes(rom_bytes.into_iter()).expect("Failed to parse BENCH_ROM into a valid ROM");
+
+        let steps = env::var("BENCH_STEPS").expect("BENCH_STEPS env var must be set for benchmarking");
+        let steps: u64 = steps.parse().unwrap();
+
+        b.iter(|| {
+            let mut nes = nes::Nes::new_from_rom(rom.clone());
+
+            for _ in 0..steps {
+                nes.step();
+            }
+        });
     }
 }
