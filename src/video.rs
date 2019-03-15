f549b2e7697fc37eb932f4d7dbf63f46c75d3659
Kyle Lacy <kylelacy+git@pm.me>
2019-03-15 14:47:26 -0700

    Add `CanvasVideo` type to enable rendering with SDL2

--- src/video.rs
+++ src/video.rs
@@ -1,3 +1,7 @@
+use sdl2::render::{Canvas, RenderTarget};
+use sdl2::pixels::Color as SdlColor;
+use sdl2::rect::Point as SdlPoint;
+
 pub struct Point {
     pub x: u16,
     pub y: u16,
@@ -20,4 +24,21 @@ impl Video for NullVideo {
     fn draw_point(&mut self, _point: Point, _color: Color) { }
     fn present(&mut self) { }
     fn clear(&mut self) { }
+}
+
+pub struct CanvasVideo<T: RenderTarget>(pub Canvas<T>);
+impl<T: RenderTarget> Video for CanvasVideo<T> {
+    fn draw_point(&mut self, point: Point, color: Color) {
+        self.0.set_draw_color(SdlColor::RGB(color.r, color.g, color.b));
+        self.0.draw_point(SdlPoint::new(point.x as i32, point.y as i32))
+            .unwrap();
+    }
+
+    fn present(&mut self) {
+        self.0.present();
+    }
+
+    fn clear(&mut self) {
+        self.0.clear();
+    }
 }
