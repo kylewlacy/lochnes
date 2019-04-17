65e7d25891036452c5e9d7b671daebe09d5c00ef
Kyle Lacy <kylelacy+git@pm.me>
2019-04-16 22:03:16 -0700

    Update `Mapper` to support ROMs with CHR RAM

--- src/nes/mapper.rs
+++ src/nes/mapper.rs
@@ -6,13 +6,15 @@ use crate::rom::Rom;
 pub struct Mapper {
     pub rom: Rom,
     work_ram: Cell<[u8; 0x2000]>,
+    chr_ram: Vec<Cell<u8>>,
 }
 
 impl Mapper {
     pub fn from_rom(rom: Rom) -> Self {
         let work_ram = Cell::new([0; 0x2000]);
+        let chr_ram = vec![Cell::new(0); rom.header.chr_ram_size_bytes];
 
-        Mapper { rom, work_ram }
+        Mapper { rom, work_ram, chr_ram }
     }
 
     pub fn read_u8(&self, addr: u16) -> u8 {
@@ -51,11 +53,18 @@ impl Mapper {
 
     pub fn read_ppu_u8(&self, nes: &Nes, addr: u16) -> u8 {
         let chr_rom = &self.rom.chr_rom;
+        let chr_ram = &self.chr_ram;
         let ppu_ram = nes.ppu.ppu_ram();
         match addr {
             0x0000..=0x1FFF => {
-                let offset = (addr as usize) % chr_rom.len();
-                chr_rom[offset]
+                if chr_rom.is_empty() {
+                    let offset = (addr as usize) % chr_ram.len();
+                    chr_ram[offset].get()
+                }
+                else {
+                    let offset = (addr as usize) % chr_rom.len();
+                    chr_rom[offset]
+                }
             }
             0x2000..=0x2FFF => {
                 let offset = ((addr - 0x2000) as usize) % ppu_ram.len();
@@ -73,8 +82,18 @@ impl Mapper {
 
     pub fn write_ppu_u8(&self, nes: &Nes, addr: u16, value: u8) {
         let ppu_ram = nes.ppu.ppu_ram();
+        let chr_rom = &self.rom.chr_rom;
+        let chr_ram = &self.chr_ram;
         match addr {
-            0x0000..=0x1FFF => { }
+            0x0000..=0x1FFF => {
+                if chr_rom.is_empty() {
+                    let offset = (addr as usize) % chr_ram.len();
+                    chr_ram[offset].set(value);
+                }
+                else {
+                    // Do nothing-- tried to write to read-only CHR ROM
+                }
+            }
             0x2000..=0x2FFF => {
                 let offset = ((addr - 0x2000) as usize) % ppu_ram.len();
                 ppu_ram[offset].set(value)
