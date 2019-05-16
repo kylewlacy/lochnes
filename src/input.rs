b4268a4b88b7ea957917d5f9cc4518e1548c4453
Kyle Lacy <kylelacy+git@pm.me>
2019-05-15 22:22:13 -0700

    Add `Input` trait with `NullInput` implementation

---
+++ src/input.rs
@@ -0,0 +1,46 @@
+use std::cell::Cell;
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct InputState {
+    pub joypad_1: JoypadState,
+    pub joypad_2: JoypadState,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct JoypadState {
+    pub a: bool,
+    pub b: bool,
+    pub start: bool,
+    pub select: bool,
+    pub up: bool,
+    pub down: bool,
+    pub left: bool,
+    pub right: bool,
+}
+
+impl Default for JoypadState {
+    fn default() -> Self {
+        JoypadState {
+            a: false,
+            b: false,
+            start: false,
+            select: false,
+            up: false,
+            down: false,
+            left: false,
+            right: false,
+        }
+    }
+}
+
+pub trait Input {
+    fn input_state(&self) -> InputState;
+}
+
+pub struct NullInput;
+
+impl Input for NullInput {
+    fn input_state(&self) -> InputState {
+        InputState::default()
+    }
+}
